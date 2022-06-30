c023243b2beefcca4405d344cb663b731c48656f
Ekocahyonoputro <108456288+Ekocahyonoputro@users.noreply.github.com>
2022-06-30 19:16:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -41,9 +41,8 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['ur number'] //ur owner number
-global.ownername = "ur name"
-global.ytname = "YT: Xeon" //ur yt chanel name
+global.owner = ['6285877664211'] //ur owner number
+global.ownername = "EKO BOT"
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
